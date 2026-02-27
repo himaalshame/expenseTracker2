64f0e42236298159cf10e6ccf6a439688ce75734
Ibrahim Ilshamy <129802781+himaalshame@users.noreply.github.com>
2026-02-27 20:17:27 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/expenseTracker2/' ,
+  base: '/expenseTracker2/', 
   plugins: [react()],
 })
